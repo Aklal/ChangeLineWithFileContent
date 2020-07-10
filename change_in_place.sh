c4451e5cc579553890289c9637f8e5c80b84e23a
aklal <oltreb@yahoo.fr>
2020-07-10 16:35:39 +0200

    Add solution

--- change_in_place.sh
+++ change_in_place.sh
@@ -17,7 +17,12 @@ function replaceTextWithFile() {
     while read line <&4 ; do
         if [[ $line =~ $regex ]];then
             echo "PATTERN FOUND"
-            sed -i '' -e '/$line/r $replacementFile' -e '//d' $originalFile
+
+            reg=${BASH_REMATCH[1]}
+            echo $reg
+            #sed -i '' -e '/$line/r $replacementFile' -e '//d' $originalFile
+            #sed -i '' -e '/$regex/r new_content' -e '//d' $originalFile
+            #sed -i -e "/$regex/r $replacementFile" -e '//d' "$originalFile"
 
             #https://stackoverflow.com/a/38782644
             # Does not work
@@ -43,7 +48,7 @@ function replaceTextWithFile() {
             #https://stackoverflow.com/a/39413525/1286214
             # Error: sed: 1: "d}
                 #": extra characters at the end of d command
-            sed -e '/^oportu.*$/{r new_content' -e 'd}' $originalFile
+            #sed -e '/^oportu.*$/{r new_content' -e 'd}' $originalFile
         fi
 
         # Following command changes the 9th line successfully
@@ -52,14 +57,14 @@ function replaceTextWithFile() {
         #the 9th line is replaced by "r $replacementFile"
         #sed -i '' -e '9s/.*/r $replacementFile/' $originalFile
 
-        content=$(cat $replacementFile)
+        #content=$(cat $replacementFile)
         #the 9th line is replaced by "$content"
         #sed -i '' -e '9s/.*/"$content"/' $originalFile
         #the 9th line is replaced by $content
         #sed -i '' -e '9s/.*/$content/' $originalFile
 
         # Try to escape particular characters in the line to replace
-        line2="\<iframe src=\"https:\/\/test\.com\/bar\/11d75d1c66667299e4fe35e\" frameborder=0\>\<\/iframe\>"
+        #line2="\<iframe src=\"https:\/\/test\.com\/bar\/11d75d1c66667299e4fe35e\" frameborder=0\>\<\/iframe\>"
         #sed -i '' -e '/$line2/r $replacementFile' -e '//d' $originalFile
 
         # Try other syntax
@@ -82,6 +87,18 @@ function replaceTextWithFile() {
         #sed -i '' -e '/"$line2"/$content' -e '//d' $originalFile
     done
     exec  4<&-
+}
+
+#Does not work
+#replaceTextWithFile
+
+#Work!!
+#https://stackoverflow.com/a/62832384/1286214
+replaceTextWithFile() {
+    local originalFile="./file.txt"
+    local regex="<iframe src=\"https:\/\/.*\" frameborder"
+    local replacementFile="./new_content"
+    sed -i '' -e "/$regex/r $replacementFile" -e '//d' "$originalFile"
 }
 
 replaceTextWithFile
